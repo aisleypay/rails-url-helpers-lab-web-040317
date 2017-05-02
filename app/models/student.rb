ced2f55203a6702f278201db607edc13acc4cf9b
aisleypay <paisley.singh@gmail.com>
2017-05-02 00:17:30 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,25 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def active_message
+   if self.active
+     output = "This student is currently active."
+   else
+     output = "This student is currently inactive."
+   end
+
+   output
+  end
+
+
+  def activate_student_path
+   if self.active == false
+     "hello false"
+     self.active = true
+   elsif self.active == true
+     "hello false"
+     self.active = true
+   end
+  end
 end
